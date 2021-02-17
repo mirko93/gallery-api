5ea833abaf54036170205f2ad643c6a4dfc4e42e
Mirko Skopljak <45762923+mirko93@users.noreply.github.com>
2021-02-17 16:46:00 +0100

    adds api route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GalleriesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// /my-galleries - /galleries
+Route::post('/galleries', [GalleriesController::class, 'store']);
+Route::patch('/galleries/{gallery}', [GalleriesController::class, 'update']);
+Route::delete('/galleries/{gallery}', [GalleriesController::class, 'destroy']);
