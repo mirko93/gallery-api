d92fa89a3a01367ca6057af16e1139d9ae62341f
Mirko Skopljak <45762923+mirko93@users.noreply.github.com>
2021-02-18 08:37:25 +0100

    adds laravel/passport and vue

---
+++ resources/js/views/Start.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>dsadasdsd</div>
+</template>
+
+<script>
+export default {
+    name: "Start"
+}
+</script>
+
+<style>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AppController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [AppController::class, 'index'])->name('home');
